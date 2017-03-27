a606c1b7ac8b4cf63fe4ad4b793ad2f1f956663f
Firede <firede@firede.us>
2017-03-27 21:08:41 +0800

    init repo.

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @file cographql-core
+ * @author Firede <firede@firede.us>
+ */
+
+const hello = (name: string): string => {
+    return `Hello ${name}!`;
+}
+
+console.info(hello('world'));
